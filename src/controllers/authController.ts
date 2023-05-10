299f5a4f67fa493bd290f590c19454d302f442ad
Fabian Trujillo <facatrusa@gmail.com>
2023-05-09 20:40:28 -0500

    V 0.0.4.1 agregada la documentacion

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -5,43 +5,48 @@ import Controller from "./controller"
 
 class authController extends Controller{
 
+    /**
+     * Autentica a un usuario y devuelve un token si la autenticación es correcta.
+     * @param req Objeto de solicitud de Express.
+     * @param res Objeto de respuesta de Express.
+     * @returns Token JWT si la autenticación es correcta.
+     */
     async autennticacion(req:Request,res:Response){
-            const tarjeta = parseInt(req.params.tarjeta)
+        const tarjeta = parseInt(req.params.tarjeta)
 
-            try {
-                const user = await this.prismaClient.medico.findFirst({
-                    where: { tarjetaProfesional: { equals: tarjeta} }
-                })
-                
-                if(!user){
-                    res.status(400).json({ message: "tarjeta profesional no existe" })
-                }else{
-                    const payload = {
-                        id:user
-                    }
-                    
-                    
-                    const options={
-                        expiresIn : "1h"
-                    }
-        
-                    const secretKey = process.env.SECRET_KEY
-        
-                    if(typeof secretKey == "string"){
-                        const token =jwt.sign(payload,secretKey,options)
-                        return res.status(201).json({ message: "Creado con exito", token})   
-                    }   
+        try {
+            // Busca al usuario con la tarjeta profesional especificada
+            const user = await this.prismaClient.medico.findFirst({
+                where: { tarjetaProfesional: { equals: tarjeta} }
+            })
+
+            if(!user){
+                // Si el usuario no existe, devuelve un error 400
+                res.status(400).json({ message: "tarjeta profesional no existe" })
+            }else{
+                const payload = {
+                    id:user
                 }
 
-                
-            } catch (error) {
-                res.status(400).json({ message: "Tarjeta profesional no existe!!!" })
-                console.log(error)
+                const options={
+                    expiresIn : "1h"
+                }
+
+                const secretKey = process.env.SECRET_KEY
+
+                if(typeof secretKey == "string"){
+                    // Si la clave secreta es una cadena, genera un token JWT y lo devuelve
+                    const token =jwt.sign(payload,secretKey,options)
+                    return res.status(201).json({ message: "Creado con exito", token})   
+                }   
             }
-            
 
-        }
+        } catch (error) {
+            // Si ocurre un error durante la autenticación, devuelve un error 400
+            res.status(400).json({ message: "Tarjeta profesional no existe!!!" })
+            console.log(error)
+        } 
     }
-
+}
 
 export default authController
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,24 +1,32 @@
 import authController from "../controllers/authController"
-import {Request,Response } from "express"
+import { Request, Response } from "express"
 import routerClass from "./routerClass"
-class authRouter extends routerClass {
 
+class authRouter extends routerClass {
     AuthController: authController
-    constructor(){
+
+    /**
+     * Crea una instancia de authRouter.
+     * @memberof authRouter
+     */
+    constructor() {
         // Llama al constructor de la clase padre
         super()
         this.AuthController = new authController()
+
         // Define las rutas para los endpoints GET y POST
         this.routes()
     }
 
-    private routes():void{
-        this.router.get("/auth/:tarjeta",
-        (req:Request, res:Response) =>{
+    /**
+     * Define las rutas para los endpoints GET y POST.
+     * @memberof authRouter
+     */
+    private routes(): void {
+        this.router.get("/auth/:tarjeta", (req: Request, res: Response) => {
             this.AuthController.autennticacion(req, res)
         })
     }
-
-
 }
+
 export default new authRouter().router
